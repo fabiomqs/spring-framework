232d284429b661c2adb43ec8fb4a1164fdd320ce
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-08-17 07:12:36 -0300

    Many to Many JPA Relationships. Closes #6.

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
@@ -0,0 +1,40 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String descripion;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripion() {
+        return descripion;
+    }
+
+    public void setDescripion(String descripion) {
+        this.descripion = descripion;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
