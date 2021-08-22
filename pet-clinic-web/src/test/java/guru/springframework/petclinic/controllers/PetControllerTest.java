fa7abd98d36d1f4d7d06940dfe3952aa0aa2c436
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-08-22 16:05:27 -0300

    Add ability to add or update visit. Closes #58.

--- pet-clinic-web/src/test/java/guru/springframework/petclinic/controllers/PetControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/petclinic/controllers/PetControllerTest.java
@@ -18,11 +18,12 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
---
+++ pet-clinic-web/src/test/java/guru/springframework/petclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,72 @@
+package guru.springframework.petclinic.controllers;
+
+import guru.springframework.petclinic.model.Pet;
+import guru.springframework.petclinic.model.Visit;
+import guru.springframework.petclinic.servies.PetService;
+import guru.springframework.petclinic.servies.VisitService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class VisitControllerTest {
+
+    @Mock
+    PetService petService;
+    @Mock
+    VisitService visitService;
+
+    @InjectMocks
+    VisitController visitController;
+
+    MockMvc mockMvc;
+    Pet pet;
+    Visit visit = new Visit();
+
+    @BeforeEach
+    void setUp() {
+        visit = new Visit();visit.setId(5L);
+        pet = new Pet();pet.setId(1L);
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        mockMvc = MockMvcBuilders.standaloneSetup(visitController).build();
+    }
+
+    @Test
+    void initNewVisitForm() throws Exception {
+        when(petService.findById(anyLong())).thenReturn(Pet.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/1/pets/2/visits/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdateVisitForm"));
+    }
+
+    @Test
+    void processNewVisitForm() throws Exception {
+        when(petService.findById(anyLong())).thenReturn(Pet.builder().id(1L).build());
+
+        mockMvc.perform(post("/owners/1/pets/2/visits/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        verify(visitService).save(any());
+
+    }
+
+}
