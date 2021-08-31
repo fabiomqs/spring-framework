4bedde5111490becf49429d7e3572b465fc42e23
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-08-31 09:22:42 -0300

    fix Mapper DI.

--- spring5-mvc-rest/src/main/java/guru/springframework/api/v1/model/mapper/CategoryMapper.java
+++ spring5-mvc-rest/src/main/java/guru/springframework/api/v1/model/mapper/CategoryMapper.java
@@ -5,7 +5,7 @@ import guru.springframework.domain.Category;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
