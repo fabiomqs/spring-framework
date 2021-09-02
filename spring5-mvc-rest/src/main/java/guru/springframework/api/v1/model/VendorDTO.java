b5e91ab5cd6553b543e94d389b68333d3c73be19
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-02 07:46:16 -0300

    Assignment: Customize Vendors. Closes #21.

--- spring5-mvc-rest/src/main/java/guru/springframework/api/v1/model/VendorDTO.java
+++ spring5-mvc-rest/src/main/java/guru/springframework/api/v1/model/VendorDTO.java
@@ -1,11 +1,13 @@
 package guru.springframework.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class VendorDTO {
 
+    @ApiModelProperty(value = "Name of the Vendor", required = true)
     private String name;
 
     @JsonProperty("vendor_url")
