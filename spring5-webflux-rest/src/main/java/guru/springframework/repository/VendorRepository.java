d6df0c67cee88ac548ab92f5e4f54fa91010434f
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-03 06:57:52 -0300

    Assignment - Create Vendor Data Model, Populate Data. Closes #23.

---
+++ spring5-webflux-rest/src/main/java/guru/springframework/repository/VendorRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.repository;
+
+import guru.springframework.domain.Vendor;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface VendorRepository extends ReactiveMongoRepository<Vendor, String> {
+}
