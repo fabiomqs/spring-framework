af38e60e1a86add972cd434c393084dc3877e576
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-03 07:32:49 -0300

    Testing Category Controller. Closes #25.

---
+++ spring5-webflux-rest/src/test/java/guru/springframework/controllers/CategoryControllerTest.java
@@ -0,0 +1,52 @@
+package guru.springframework.controllers;
+
+import guru.springframework.controllers.v1.CategoryController;
+import guru.springframework.domain.Category;
+import guru.springframework.repository.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    public void list() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(Category.builder().description("Cat1").build(),
+                        Category.builder().description("Cat2").build()));
+
+        webTestClient.get()
+                .uri(CategoryController.BASE_URL)
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getById() {
+        BDDMockito.given(categoryRepository.findById("someid"))
+                .willReturn(Mono.just(Category.builder().description("Cat").build()));
+
+        webTestClient.get()
+                .uri(CategoryController.BASE_URL + "/someid")
+                .exchange()
+                .expectBody(Category.class);
+
+    }
+}
