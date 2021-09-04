570ff3a21d8d251d37f95b665206b2b5a7daf4e2
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-04 11:00:45 -0300

    Java Message Object. Closes #51

---
+++ jms/src/main/java/guru/springframework/jms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package guru.springframework.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -8708305855930327096L;
+
+    private UUID id;
+    private String message;
+}
