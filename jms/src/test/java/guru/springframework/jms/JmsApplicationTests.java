92659a935d59b5d6c23f4e4d86fce8c62f77b64e
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-04 10:46:26 -0300

    spring initlizr project

---
+++ jms/src/test/java/guru/springframework/jms/JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
